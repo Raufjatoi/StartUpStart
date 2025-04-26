032696a27720c2cd9e9d806591b2e9a526b84872
Abdul Rauf <141711142+Raufjatoi@users.noreply.github.com>
2025-04-26 21:21:41 +0500

    few changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
@@ -18,4 +18,5 @@ export default defineConfig({
     },
   },
 });
+
 
